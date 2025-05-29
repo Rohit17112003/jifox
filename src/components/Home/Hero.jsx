b860b01706ba6e24f2e597dce53d4ba55a168939
Rohit17112003 <133210462+Rohit17112003@users.noreply.github.com>
2025-05-29 14:40:17 +0530

    testing some text in hero  page

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,7 +1,7 @@
 const Hero = () => {
   const design = (
     <>
-      <div>
+      <div className="px-4 md:px-36 text-blue">
         <h1>Welcome to hero page Mister Rohit Rana</h1>
         <h1>Welcome to hero page Mister Rohit Rana</h1>
         <h1>Welcome to hero page Mister Rohit Rana</h1>
