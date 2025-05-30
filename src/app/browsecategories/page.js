825118d4ae4cf643373fb965010390355ac90c4b
Rohit17112003 <133210462+Rohit17112003@users.noreply.github.com>
2025-05-30 16:39:09 +0530

    currection of browser page

--- src/app/browsecategories/page.js
+++ src/app/browsecategories/page.js
@@ -46,63 +46,69 @@ const page = () => {
   ];
   const design = (
     <>
-      <div className="grid grid-cols-4 px-4 md:px-36 pt-10 md:gap-7 gap-5 items-center">
+      <div className="grid md:grid-cols-4 grid-cols-3 px-4 md:px-36 pt-10 md:gap-7 gap-5 items-center">
         <div className="col-span-1 flex flex-col gap-4 text-[0.9rem]">
           <h1 className="text-[1.85rem] font-semibold">Filters</h1>
         </div>
-        <div className="col-span-3 grid grid-cols-3 md:gap-5 gap-3 py-1 md:py-0">
+        <div className="md:col-span-3 col-span-2 grid md:grid-cols-3 grid-cols-2 md:gap-5 gap-3 py-1 md:py-0">
           <h1 className="text-[1.4rem] font-semibold">Pants</h1>
         </div>
       </div>
 
-      <div className="grid grid-cols-4 px-4 md:px-36 md:gap-7 gap-5 pt-4 pb-8">
+      <div className="grid md:grid-cols-4 grid-cols-3 px-4 md:px-36 md:gap-7 gap-5 pt-4 pb-8">
         <div className="col-span-1 flex flex-col gap-4 text-[0.9rem]">
-          <div className="flex justify-between items-center text-white bg-[#222222] px-4 py-2.5">
+          <div className="flex justify-between items-center text-white bg-[#222222] md:px-4 px-2 py-2.5">
             <p>Sort By</p>
             <span>
               <FaPlus />
             </span>
           </div>
-          <div className="flex justify-between items-center text-white bg-[#222222] px-4 py-2.5">
+          <div className="flex justify-between items-center text-white bg-[#222222] md:px-4 px-2 py-2.5">
             <p>Size</p>
             <span>
               <FaPlus />
             </span>
           </div>
-          <div className="flex justify-between items-center text-white bg-[#222222] px-4 py-2.5">
+          <div className="flex justify-between items-center text-white bg-[#222222] md:px-4 px-2 py-2.5">
             <p>Color</p>
             <span>
               <FaPlus />
             </span>
           </div>
-          <div className="flex justify-between items-center text-white bg-[#222222] px-4 py-2.5">
+          <div className="flex justify-between items-center text-white bg-[#222222] md:px-4 px-2 py-2.5">
             <p>Collection</p>
             <span>
               <FaPlus />
             </span>
           </div>
-          <div className="flex justify-between items-center text-white bg-[#222222] px-4 py-2.5">
+          <div className="flex justify-between items-center text-white bg-[#222222] md:px-4 px-2 py-2.5">
             <p>Febric</p>
             <span>
               <FaPlus />
             </span>
           </div>
         </div>
-        <div className="col-span-3 grid grid-cols-3 md:gap-5 gap-3 py-1 md:py-0">
+        <div className="md:col-span-3 col-span-2 grid md:grid-cols-3 grid-cols-2 md:gap-5 gap-3 py-1 md:py-0">
           {productList.map((items) => (
             <div key={items.id || index}>
               <img src={items.image} alt="cart" className="w-full h-full" />
             </div>
           ))}
           <div className="flex justify-center py-6">
-            <button className="hidden border border-black no-underline px-4 py-1.5 rounded-md mt-2  hover:bg-black hover:text-white">
+            <a
+              href="/"
+              className="md:hidden block border border-black no-underline px-3 py-1.5 rounded-md mt-2  hover:bg-black hover:text-white"
+            >
               Load More
-            </button>
+            </a>
           </div>
-           <div className="flex justify-center py-6">
-            <button className=" border border-black no-underline px-4 py-1.5 rounded-md mt-2  hover:bg-black hover:text-white">
+          <div className="flex justify-center py-6">
+            <a
+              href="/"
+              className=" md:block hidden border border-black no-underline px-4 py-1.5 rounded-md mt-2  hover:bg-black hover:text-white"
+            >
               Load More
-            </button>
+            </a>
           </div>
         </div>
       </div>
