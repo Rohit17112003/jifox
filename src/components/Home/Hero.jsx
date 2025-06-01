5ae30d125b1253216ac7666f837f68098cda17a5
Rohit17112003 <133210462+Rohit17112003@users.noreply.github.com>
2025-06-01 14:46:21 +0530

    again

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
         <div className="bg-[#F6F6F6] md:px-0 px-4 flex items-center md:py-0 py-5 relative">
           <div className=" bg-[#EEEEEE] rounded-lg py-10 pb-12 px-8 w-full md:ml-36 text-black md:-mr-24">
             <p className="text-[1.06rem]">SUMMER SALE 50% OFF rohit</p>
-            <p className="font_Architects_Daughter md:text-5xl text-3xl md:py-6 py-4  tracking-wider font-normal">Design For Beauty</p>
+            <p className="font_Architects_Daughter md:text-5xl text-3xl md:py-6 py-4  tracking-wider font-normal">Design For Beauty rohit</p>
             <p className="text-[1.06rem] pb-3.5">You Can Have Anything You Want In Life You Dress For It. </p>
             <a href="/browsecategories" className="border border-black no-underline px-4 py-2 rounded-md shadow-sm shadow-green-200 hover:bg-black hover:text-white">
               All Collection
